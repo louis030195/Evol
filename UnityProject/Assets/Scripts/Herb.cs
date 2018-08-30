de6480f92d57a1b8e0710a1ea9c82ae86b90743c
U-DESKTOP-V50FEUE\louis-PC <louis.beaumont@gmail.com>
2018-08-30 23:13:13 +0200

    Fix herb spawn

--- UnityProject/Assets/Scripts/Herb.cs
+++ UnityProject/Assets/Scripts/Herb.cs
@@ -4,15 +4,23 @@
 /// This class handle the behaviour of the herb gameobject
 /// </summary>
 public class Herb : MonoBehaviour {
-    
+
+    float groundSize;
+    float offsetX;
+
+    private void Start()
+    {
+        groundSize = transform.parent.Find("Ground").GetComponent<MeshRenderer>().bounds.size.x / 2;
+        offsetX = transform.parent.position.x;
+        transform.position = new Vector3(Random.Range(-groundSize, groundSize) + offsetX, transform.position.y,
+            Random.Range(-groundSize, groundSize));
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         
         if(collision.collider.GetComponent<HerbivorousAgent>() != null)
         {
-            float groundSize = transform.parent.Find("Ground").GetComponent<MeshRenderer>().bounds.size.x / 2;
-            float offsetX = transform.parent.position.x;
             transform.position = new Vector3(Random.Range(-groundSize, groundSize) + offsetX, transform.position.y,
                 Random.Range(-groundSize, groundSize));
         }
