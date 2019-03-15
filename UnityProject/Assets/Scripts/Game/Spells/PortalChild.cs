0d244a362d09ba9ead61f2c2f04c8fdc689d29e3
Aubin Porte <aubin.porte@gmail.com>
2019-03-15 15:22:45 +0100

    Portail system

---
+++ UnityProject/Assets/Scripts/Game/Spells/PortalChild.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Evol.Game.Spell
+{
+	public class PortalChild : MonoBehaviour {
+
+		private void OnCollisionEnter(Collision other)
+		{
+			GetComponentInParent<Portal>().TeleportPosition(other.transform);
+		}
+	}
+}
