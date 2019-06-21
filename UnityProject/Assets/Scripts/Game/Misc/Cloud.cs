0d43636bae041e226689470ea0df93483279c0d9
louis030195 <louis.beaumont@gmail.com>
2019-06-21 16:02:57 +0200

    Add cloud movement, few fixes

---
+++ UnityProject/Assets/Scripts/Game/Misc/Cloud.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Evol.Game.Misc
+{
+    public class Cloud : MonoBehaviour
+    {
+        //Set these variables to whatever you want the slowest and fastest speed for the clouds to be, through the inspector.
+        //If you don't want clouds to have randomized speed, just set both of these to the same number.
+        //For Example, I have these set to 2 and 5
+        private float minSpeed = 2;
+        private float maxSpeed = 7;
+
+        //Set these variables to the lowest and highest y values you want clouds to spawn at.
+        //For Example, I have these set to 1 and 4
+        private float minY = 50f;
+        private float maxY = 90f;
+        
+        
+        private Color alphaColor;
+        private float timeToFade = 10.0f;
+
+        private float speed;
+        private float camWidth;
+
+        private void Start()
+        {
+            alphaColor = GetComponent<MeshRenderer>().material.color;
+            alphaColor.a = 0;
+            transform.position = new Vector3(Random.Range(-300, 300), Random.Range(minY, maxY), Random.Range(-300, 300));
+            //Set Cloud Movement Speed, and Position to random values within range defined above
+            speed = Random.Range(minSpeed, maxSpeed);
+            // GetComponent<MeshRenderer>().material.color = Color.Lerp(alphaColor, GetComponent<MeshRenderer>().material.color, timeToFade * Time.deltaTime);
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            //Translates the cloud to the right at the speed that is selected
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+            //If cloud is off Screen, Destroy it.
+            if (transform.position.z < -100)
+            {
+                //Destroy(gameObject, timeToFade);
+                GetComponent<MeshRenderer>().material.color = Color.Lerp(GetComponent<MeshRenderer>().material.color, alphaColor, timeToFade * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Game/Misc/CloudManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Evol.Utils;
+
+namespace Evol.Game.Misc
+{
+    public class CloudManager : MonoBehaviour
+    {
+        //Set this variable to your Cloud Prefab through the Inspector
+        public GameObject[] cloudPrefabs;
+
+        //Set this variable to how often you want the Cloud Manager to make clouds in seconds.
+        //For Example, I have this set to 2
+        public float delay = 2f;
+
+        //If you ever need the clouds to stop spawning, set this variable to false, by doing: CloudManagerScript.spawnClouds = false;
+        public static bool spawnClouds = true;
+
+        // Use this for initialization
+        void Start()
+        {
+            //Begin SpawnClouds Coroutine
+            StartCoroutine(SpawnClouds());
+        }
+
+        IEnumerator SpawnClouds()
+        {
+            //This will always run
+            while (true)
+            {
+                //Only spawn clouds if the boolean spawnClouds is true
+                while (spawnClouds)
+                {
+                    var randomRotation = Random.Range(0, 360);
+                    //Instantiate Cloud Prefab and then wait for specified delay, and then repeat
+                    Instantiate(cloudPrefabs.PickRandom(), Vector3.zero, new Quaternion(0, randomRotation, 0, randomRotation));
+                    yield return new WaitForSeconds(delay);
+                }
+            }
+        }
+    }
+}
